ad9e46562cec9d380248e367632845f5a034378b
Ramon Iñiguez <44062098+riniguez91@users.noreply.github.com>
2021-11-25 10:31:55 +0100

    Fixed test graph issues

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import streamlit as st
+import pandas as pd
 import requests
 import json
+import numpy as np
 
 def test_graph(data):
-    st.write(data)
+    res = json.loads(data)
+    for idx,i in enumerate(res):
+        res[idx] = json.loads(i)
+    st.write(res)
 
-def main():
-    if __name__ == '__main__':
-        r = json.loads(requests.get(url='http://127.0.0.1:5000/api/test'))
-        test_graph(r)
+    df = pd.DataFrame(res).astype(str)
+    df = df.set_index('SECTOR')
+    df['count'] = pd.to_numeric(df['count'])
+    st.bar_chart(df)
+
+
+if __name__ == '__main__':
+    r = requests.get(url='http://127.0.0.1:5000/api/test').content
+    test_graph(r)
         
 
